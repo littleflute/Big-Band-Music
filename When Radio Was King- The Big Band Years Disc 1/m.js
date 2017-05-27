b99e222a449a9d8b53155d9bd471b08a811daf02
littleflute <13699175164@sina.cn>
2017-05-27 15:40:24 -0500

    Add files via upload

---
+++ When Radio Was King- The Big Band Years Disc 1/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 Begin the Beguine.mp3";		
+f[2]	= "02 Woodchopper's Ball.mp3";		
+f[3]	= "03 Stompin' at the Savoy.mp3";		
+f[4]	= "04 One O'Clock Jump.mp3";		
+f[5]	= "05 Pennsylvania 6-5000.mp3";		
+f[6]	= "06 It's Only a Paper Moon.mp3";		
+f[7]	= "07 Flying Home.mp3";		
+f[8]	= "08 On the Sunny Side of the Street.mp3";		
+f[9]	= "09 September Song.mp3";		
+f[10]	= "10 Mean To Me.mp3";		
+f[11]	= "11 You Made Me Love you.mp3";		
+f[12]	= "12 Cherokee.mp3";		
+f[13]	= "13 Opus One.mp3";		
+f[14]	= "14 Indian Love Call.mp3";		
+f[15]	= "15 Take the 'A' Train.mp3";		
